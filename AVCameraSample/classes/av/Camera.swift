6b8006c6ba349ba8e449a9ba2c3127629ace3657
Kohei Tabata <nerd0geek1@gmail.com>
2016-02-14 18:38:48 +0900

    add Camera class

---
+++ AVCameraSample/classes/av/Camera.swift
@@ -0,0 +1,13 @@
+//
+//  Camera.swift
+//  AVCameraSample
+//
+//  Created by Kohei Tabata on 2016/02/14.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import Foundation
+
+class Camera: NSObject {
+
+}
