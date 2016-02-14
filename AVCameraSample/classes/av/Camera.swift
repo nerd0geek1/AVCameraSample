ce1fa542e53dfef21d3120a2b957cf5723fd53b8
Kohei Tabata <nerd0geek1@gmail.com>
2016-02-14 23:41:28 +0900

    implement Camera feature using AVFoundation

--- AVCameraSample/classes/av/Camera.swift
+++ AVCameraSample/classes/av/Camera.swift
@@ -6,8 +6,87 @@
 //  Copyright © 2016年 Kohei Tabata. All rights reserved.
 //
 
+import AVFoundation
 import Foundation
 
-class Camera: NSObject {
+class Camera: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    static let sharedInstance: Camera = Camera()
+
+    var didCaptureSampleBufferClosure: ((CMSampleBuffer) -> Void)?
+
+    private let captureSession: AVCaptureSession = AVCaptureSession()
+    private var device: AVCaptureDevice!
+
+    override private init() {
+        super.init()
+
+        setup()
+    }
+
+    //MARK: - private
+
+    private func setup() {
+        guard let devices = AVCaptureDevice.devices() as? [AVCaptureDevice] else {
+            return
+        }
+
+        for device in devices {
+            if !device.hasMediaType(AVMediaTypeVideo) {
+                continue
+            }
+
+            if device.position == .Front {
+                self.device = device
+                break
+            }
+        }
+
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+
+        do {
+            let deviceInput: AVCaptureInput = try AVCaptureDeviceInput(device: device)
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            }
+
+            let videoOutput: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey : Int(kCVPixelFormatType_32BGRA)]
+
+            let videoOutputQueue: dispatch_queue_t = dispatch_queue_create("AVCameraSample.Camera.videoOutputQueue", nil)
+            videoOutput.setSampleBufferDelegate(self, queue: videoOutputQueue)
+            if captureSession.canAddOutput(videoOutput) {
+                captureSession.addOutput(videoOutput)
+            }
+
+            var videoConnection: AVCaptureConnection? = nil
+            guard let connections: [AVCaptureConnection] = videoOutput.connections as? [AVCaptureConnection] else {
+                return
+            }
+
+            captureSession.beginConfiguration()
+            for connection in connections {
+                guard let inputPorts: [AVCaptureInputPort] = connection.inputPorts as? [AVCaptureInputPort] else {
+                    return
+                }
+                for port in inputPorts {
+                    if port.mediaType == AVMediaTypeVideo {
+                        videoConnection = connection
+                        break
+                    }
+                }
+            }
+
+            videoConnection?.videoOrientation = .Portrait
+
+            captureSession.commitConfiguration()
+            captureSession.startRunning()
+        } catch let error as NSError {
+            print("error:\(error)")
+        }
+    }
+
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        didCaptureSampleBufferClosure?(sampleBuffer)
+    }
 }
--- AVCameraSample/classes/viewcontroller/ViewController.swift
+++ AVCameraSample/classes/viewcontroller/ViewController.swift
@@ -7,10 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let imageView: UIImageView = UIImageView(frame: UIScreen.mainScreen().bounds)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupImageView()
+        setupCamera()
+    }
+
+    //MARK: - private
+
+    private func setupImageView() {
+        view.addSubview(imageView)
+    }
+
+    private func setupCamera() {
+        Camera.sharedInstance.didCaptureSampleBufferClosure = {[weak self] sampleBuffer -> Void in
+            dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                self?.imageView.image = self?.convertSampleBufferToUIImage(sampleBuffer)
+            })
+        }
+    }
+
+
+    //MARK: - convert CMSampleBuffer to UIImage
+
+    private func convertSampleBufferToUIImage(sampleBuffer: CMSampleBufferRef) -> UIImage {
+        let pixelBuffer: CVImageBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        let ciImage: CIImage              = CIImage(CVPixelBuffer: pixelBuffer)
+        let image                         = UIImage(CIImage: ciImage)
+        return image
     }
 }
