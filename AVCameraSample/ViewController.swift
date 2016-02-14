3522d2c2386455ec48e82bef9202dd2a71c4e1cc
Kohei Tabata <nerd0geek1@gmail.com>
2016-02-14 18:24:27 +0900

    Initial commit

---
+++ AVCameraSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVCameraSample
+//
+//  Created by Kohei Tabata on 2016/02/14.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
